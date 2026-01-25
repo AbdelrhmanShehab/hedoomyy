4f13dcc0192ed204c136f4cd7f54d65ddcbc9e6c
unknown <abdlrhman.shihab@gmail.com>
2026-01-25 21:36:51 +0200

    pages structure

---
+++ app/account/page.tsx
@@ -0,0 +1,8 @@
+export default function AccountPage() {
+    return (
+        <div>
+            <h1>Your Account</h1>
+            {/* Account content goes here */}
+        </div>
+    );
+}
---
+++ app/favorites/page.tsx
@@ -0,0 +1,8 @@
+export default function FavoritesPage() {
+  return (
+    <div>
+      <h1>Your Favorites</h1>
+      {/* Favorites content goes here */}
+    </div>
+  );
+}
